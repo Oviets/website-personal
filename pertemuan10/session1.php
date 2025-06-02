440b272ef837ef4aee779a14a3052d7316dde032
Oviets <ovitabdullah@gmail.com>
2025-06-02 21:56:40 +0700

    Add files via upload

---
+++ pertemuan10/session1.php
@@ -0,0 +1,38 @@
+<?php
+// Nama file : session1.php
+// Halaman ini merupakan halaman contoh penciptaan session. 
+// Perintah session_start() harus ditaruh diperintah pertama tanpa spasi di depannya. 
+// Perintah session_start() harus ada pada setiap halaman yang berhubungan dengan session
+
+session_start();
+if (isset($_POST['Login'])) {
+    $user = $_POST['user'];
+    $pass = $_POST['pass'];
+    // Periksa login
+    if ($user == "mahasiswa" && $pass == "@mahasiswa") {
+        // Menciptakan session
+        $_SESSION['login'] = $user;
+        // Menuju ke halaman pemeriksaan session
+        echo "<h1>Anda berhasil LOGIN</h1>";
+        echo "<h2>Klik <a href='session2.php'>di sini (session2.php)</a> 
+              untuk menuju ke halaman pemeriksaan session </h2>";
+    } else {
+        echo "<h1>Login gagal</h1>";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Silahkan Login ...</title>
+</head>
+<body>
+    <h2>Silahkan Login ...</h2>
+    <form action="" method="post">
+        Username : <input type="text" name="user"/><br/>
+        Password : <input type="password" name="pass"/><br/>
+        <input type="submit" name="Login" value="Log In"/>
+    </form>
+</body>
+</html>
